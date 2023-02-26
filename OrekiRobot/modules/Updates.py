fdf588587be813712a2035452d55cf56997c0b0a
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-02-26 19:01:44 +0530

    Update Updates.py

--- OrekiRobot/modules/Updates.py
+++ OrekiRobot/modules/Updates.py
@@ -7,16 +7,12 @@ PHOTO = "https://te.legra.ph/file/c4b3a0fb319744a2e41fd.jpg"
 @register(pattern=("/updates"))
 async def awake(event):
     OREKI = """
-    
-    
-    ⊛ ✅ ⊛
-    * Welcome Messages Are Changed✅ *
-    * Music Feature Coming Tomorrow ✅ *
+    ⊛ New Fonts Module Added✅ ⊛
+    ⊛ New Google Module Added✅ ⊛
+    ⊛ Image Searching Module Added✅ ⊛
     * Running On New Repo✅ *
     * Almost All Errors Fixed ✅ *
     * UI Changed ✅ *
     * You can Rename Files ✅ *
 """
     await oreki.send_file(event.chat_id, PHOTO, caption=OREKI)
-
-
