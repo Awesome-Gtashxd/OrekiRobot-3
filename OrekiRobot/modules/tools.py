fef709855229540026ff4f2dcb2cbbdb71e0f285
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-02-27 17:27:11 +0530

    Update tools.py

--- OrekiRobot/modules/tools.py
+++ OrekiRobot/modules/tools.py
@@ -12,7 +12,7 @@ from PIL import Image
 from search_engine_parser import GoogleSearch
 
 from OrekiRobot import tbot as oreki
-from OrekiRobot.events import register as bot
+from OrekiRobot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
