918c72e25aeba6531015d8b1224d0afeb11ac25b
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-03-01 20:28:49 +0530

    Update Updates.py

--- OrekiRobot/modules/Updates.py
+++ OrekiRobot/modules/Updates.py
@@ -7,15 +7,11 @@ PHOTO = "https://te.legra.ph/file/d9a595877810a3c73caa8.jpg"
 @register(pattern=("/updates"))
 async def awake(event):
     OREKI = """
-⊛ New Fonts Module Added✅ ⊛
 ⊛ New Google Module Added✅ ⊛
-**⊛ New Image Searching Module Added✅ ⊛**
-**⊛ New Zip/Unzip Module Added✅ ⊛**
-⊛ Almost All Errors Fixed✅ ⊛
-**⊛ Pm/Dm Start was Updated✅ ⊛**
-**⊛ New Waifu Module Added✅ ⊛**
-**⊛ Youtube Video & Song Uploader Module Added✅ ⊛**
-**⊛ Special Update: New Files Renamer Module Added✅ ⊛**
+⊛ New Image Searching Module Added✅ ⊛
+⊛ New Zip/Unzip Module Added✅ ⊛
+⊛ Pm/Dm Start was Updated✅ ⊛
+⊛ Youtube Video & Song Uploader Module Added✅ ⊛
 
       **Next Update is Comming on 20th March**
 """
