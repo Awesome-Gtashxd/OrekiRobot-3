3f6f33469766a9884cb87aaea09950ef9c934059
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-03-01 11:23:15 +0530

    Update alive.py

--- OrekiRobot/modules/alive.py
+++ OrekiRobot/modules/alive.py
@@ -5,18 +5,18 @@ from telethon import __version__ as tlhver
 
 from OrekiRobot import BOT_NAME
 from OrekiRobot import tbot as oreki
-from OrekiRobot.events import register as oreki
+from OrekiRobot.events import register
 
 IMAGE = "https://te.legra.ph/file/344cdfc69c65647c10313.jpg"
 
 
-@oreki(pattern=("/alive"))
+@register(pattern=("/alive"))
 async def awake(event):
     OREKI = """
 **Hola I'm {BOT_NAME} ~ 🖤!**
-**Python-Telegram-Bot Version ~ 🖤 :** `{ptb}`\n\n
-**Telethon Version ~ 🖤:** `{tlhver}`\n\n
-**Pyrogram Version ~ 🖤:** `{pyrover}`\n\n
+**Python-Telegram-Bot Version ~ 🖤 :** {ptb}
+**Telethon Version ~ 🖤:** {tlhver}
+**Pyrogram Version ~ 🖤:** {pyrover}
 **My Master ~ 🖤 :** [THE GTASH](https://t.me/Awesome_Gtashxd)
 """
 
@@ -24,11 +24,6 @@ async def awake(event):
         [
             Button.url(
                 "Add {BOT_NAME} To Your Group ✅", "https://t.me/{BOT_NAME}?startgroup=true"
-            ),
-    ],
-    [
-            Button.url(
-                "Developer🎗", "https://t.me/Awesome_Gtashxd"
             ),
     ],
     [
