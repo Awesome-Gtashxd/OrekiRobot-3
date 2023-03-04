8170ef8452adf1086a73ba7ce68d88407f713e35
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-03-04 19:38:26 +0530

    Update __init__.py

--- OrekiRobot/modules/movies/__init__.py
+++ OrekiRobot/modules/movies/__init__.py
@@ -1,1 +1,9 @@
+import sys
 
+from OrekiRobot import LOGGER
+
+except Exception as e:
+    LOGGER.exception(f"Movies Failed to upload here")
+    sys.exit()
+
+LOGGER.info("Movies Connection successful, session started.")
