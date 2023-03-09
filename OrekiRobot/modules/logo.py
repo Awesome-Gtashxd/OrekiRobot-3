e141a30c7765b091af7201d36d6ce49025a89c7b
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-03-09 13:14:25 +0530

    Update logo.py

--- OrekiRobot/modules/logo.py
+++ OrekiRobot/modules/logo.py
@@ -261,7 +261,7 @@ async def lego(event):
         img = Image.open(io.BytesIO(requests.get(randc).content))
         draw = ImageDraw.Draw(img)
         image_widthz, image_heightz = img.size
-        fnt = glob.glob("./OrekiRobot/LOGO_FONT/*")
+        fnt = glob.glob("./OrekiRobot/Fonts/*")
         randf = random.choice(fnt)
         font = ImageFont.truetype(randf, 120)
         w, h = draw.textsize(text, font=font)
