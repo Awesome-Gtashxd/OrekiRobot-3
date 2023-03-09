36d225c73737560fb6aeaf4acf8acf63d847ec07
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-03-09 13:21:52 +0530

    Create movies.py

---
+++ OrekiRobot/modules/movies.py
@@ -0,0 +1,17 @@
+import html
+import random
+import time
+
+from telegram import ParseMode, Update, ChatPermissions
+from telegram.ext import CallbackContext, run_async
+from telegram.error import BadRequest
+
+import OrekiRobot.modules.movies.movie_strings as movie_strings
+from OrekiRobot.modules.helper_funcs.chat_status import (is_user_admin)
+from OrekiRobot.modules.helper_funcs.extraction import extract_user
+
+
+
+@run_async
+def thunivu(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(movie_strings.THUNIVU_STRINGS))
