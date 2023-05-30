d141a76aa47bff45c4989701ac4a53ea1d1c34ef
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-05-31 00:54:54 +0530

    Update __main__.py

--- OrekiRobot/__main__.py
+++ OrekiRobot/__main__.py
@@ -274,7 +274,14 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_animation(GROUP_START_IMG),
-            caption=f"𝐇eyoo, {first}, 𝐈 𝐚m 𝐎reki, 𝐓he 𝐏rince💕../n➖➖➖➖➖➖➖➖➖➖➖➖➖/n• I am a anime themed bot i can help you to Manage ur all groups../n• I am a Prince in my World/n➖➖➖➖➖➖➖➖➖➖➖➖➖/n𝐖anna 𝐊now 𝐌y 𝐂ommands? 𝐇it /help",
+            caption=f"𝐇eyoo, {first}, 𝐈 𝐚m 𝐎reki, 𝐓he 𝐏rince💕..
+                      ➖➖➖➖➖➖➖➖➖➖➖➖➖
+                      • I am a anime themed bot i can help you
+                        to Manage ur all groups..
+                        
+                      • I am a Prince in my World
+                      ➖➖➖➖➖➖➖➖➖➖➖➖➖
+                      𝐖anna 𝐊now 𝐌y 𝐂ommands? 𝐇it /help",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
