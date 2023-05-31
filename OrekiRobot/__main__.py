61b0f9b88fbd3b2e755ce2510d326bbc847f5ed8
The Gtash <99383665+Awesome-Gtashxd@users.noreply.github.com>
2023-05-31 10:19:43 +0530

    Update __main__.py

--- OrekiRobot/__main__.py
+++ OrekiRobot/__main__.py
@@ -271,7 +271,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_animation(GROUP_START_IMG),
-            caption=f"<b>I'm Alive!\n<b>Not sleep since:</b> <code>{uptime}</code>",
+            caption=f"<b>Hey, I'm alive!\nNot sleep since:</b> <code>{uptime}</code>",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
